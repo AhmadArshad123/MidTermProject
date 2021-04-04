32a47b17a9b69b25f06df1c720739dcc6baae877
AhmadArshad123 <ahmadjutt454979@gmail.com>
2021-04-05 00:24:24 +0500

    MidTermProject

---
+++ rubricdetail/src/rubricmanagement/Rubric.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubricmanagement;
+
+/**
+ *
+ * @author hp
+ */
+public class Rubric {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO         MainFrame.setVisible(true);
+//code application logic here
+        Frame MainFrame = new Frame();
+    }
+    
+}
